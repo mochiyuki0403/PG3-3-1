9e31f2306fa8cf4bb66fd952ab3a5cebddcf6b84
mochiyuki0403 <146901463+mochiyuki0403@users.noreply.github.com>
2024-08-02 14:50:26 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<iostream>
+#include<list>
+
+using namespace std;
+int main() {
+	list<const char*>ekiname = { "tokyo","kanda","akihabara","okachimachi","ueno"
+		,"uguisudani","nippori","tabata","komagome"
+		,"sugamo","otuka","ikebukuro","mejiro","takadanobaba",
+		"shino-kubo","shinjuku","yoyogi"
+		,"harajuku","shibuya","ebisu","meguro","gotanda","osaki"
+		,"shinagawa","tamachi","hamamatucho","shinbashi","yurakucho" };
+	for (list<const char*>::iterator itr = ekiname.begin(); itr != ekiname.end(); ++itr) {
+		cout << *itr << endl;
+	}
+	for (list<const char*>::iterator itr = ekiname.begin(); itr != ekiname.end(); ++itr) {
+		if (strcmp(*itr, "tabata") == 0) {
+			itr = ekiname.insert(itr, "nishi nippori"); ++itr;
+		}
+	}
+	printf("\n2019\n");
+	for (list<const char*>::iterator itr = ekiname.begin(); itr != ekiname.end(); ++itr) {
+		cout << *itr << endl;
+	}
+	printf("\n2022\n");
+	for (list<const char*>::iterator itr = ekiname.begin(); itr != ekiname.end(); ++itr) {
+		if (strcmp(*itr, "tamachi") == 0) {
+			itr = ekiname.insert(itr, "takanawa getoway"); ++itr;
+		}
+	}
+	for (list<const char*>::iterator itr = ekiname.begin(); itr != ekiname.end(); ++itr) {
+		cout << *itr << endl;
+	}
+	return 0;
+}
